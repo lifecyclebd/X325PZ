7f98e711e281d9b7b463b6ac6b7461f953069bb3
jmrashed <jmrashed@gmail.com>
2017-07-21 21:38:32 +0600

    admin home

--- resources/views/leftsidebar.blade.php
+++ resources/views/leftsidebar.blade.php
@@ -12,22 +12,12 @@
                 <a href="{{ url('/profile')}}"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                    </button>
-                </span>
-            </div>
-        </form>
+      
         
            
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li> 
+        <ul class="sidebar-menu"> 
             <li class="active"><a href="{{ url('/admin/home')}}"><i class="fa fa-home"></i> <span>Home</span></a></li> 
             <li class="treeview">
                 <a href="{{ url('/admin/view') }}">
--- storage/framework/views/6bed57215d6aa62628712078a2422c7a062345d4.php
+++ storage/framework/views/6bed57215d6aa62628712078a2422c7a062345d4.php
@@ -12,22 +12,12 @@
                 <a href="<?php echo e(url('/profile')); ?>"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                    </button>
-                </span>
-            </div>
-        </form>
+      
         
            
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li> 
+        <ul class="sidebar-menu"> 
             <li class="active"><a href="<?php echo e(url('/admin/home')); ?>"><i class="fa fa-home"></i> <span>Home</span></a></li> 
             <li class="treeview">
                 <a href="<?php echo e(url('/admin/view')); ?>">
@@ -192,7 +182,7 @@
                 </ul>
             </li>
 
- 
+            <li class="active"><a href="<?php echo e(url('/admin/more-blood/view')); ?>"><i class="fa fa-home"></i> <span>More About Blood</span></a></li> 
 
             <li class="treeview">
                 <a href="<?php echo e(url('/content')); ?>">
