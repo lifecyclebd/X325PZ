66134b5129f9cfbef9ab08963b0fe3aa0bac5455
Syeda Tasnim Meem <Syeda Tasnim Meem>
2017-07-29 19:43:24 +0600

    setup desktop

--- storage/framework/views/5bc43dc0e26bd52854f61ebea8e5e8323d9df871.php
+++ storage/framework/views/5bc43dc0e26bd52854f61ebea8e5e8323d9df871.php
@@ -12,7 +12,7 @@
 
 
 
-
+<?php echo $__env->make('frontend.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 
 
 <?php echo $__env->yieldContent('script_link'); ?>
@@ -20,29 +20,56 @@
 
 <?php echo $__env->yieldContent('javascript_code'); ?>
 
-<?php echo $__env->make('frontend.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 
 
 
 
 
 
+<!-- /.content-wrapper -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".divisions").change(function () {
+            var classid = $(this).val();
+            // alert(classid);
+            $("#districts").html('');
+            $.ajax({
+                url: "<?php echo e(url('admin/donor/get_district')); ?>" + '/' + classid,
+                type: "GET",
+                dataType: "html",
+                success: function (data) {
+                    //    alert(data);
+                    $("#districts").append(data);
+                }
+            });
+        });
+    });
 
 
+    $(document).ready(function () {
+        $(".districts").change(function () {
+            var classid = $(this).val();
+            // alert(classid);
+            $("#upazillas").html('');
+            $.ajax({
+                url: "<?php echo e(url('admin/donor/get_upazilla')); ?>" + '/' + classid,
+                type: "GET",
+                dataType: "html",
+                success: function (data) {
+                    // alert(data);
+                    $("#upazillas").append(data);
+                }
+            });
+        });
+    });
 
+       $(document).ready(function () {
+        $(".is_active").click(function () {
+              $(this).addClass("active");
+        });
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   </script>
+   
+</body>
+</html>
